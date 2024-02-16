d214e2a9c5c14cdfcc9e91391cc4e9074a31d468
PinkyGraal <pinkyplaysgraal@gmail.com>
2024-02-16 02:21:54 -0700

    Finished Giveaway Page

---
+++ Google Sheet.js
@@ -0,0 +1,65 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Perform validation
+    var graalID = document.getElementById('graalID').value.trim();
+    var inGameName = document.getElementById('inGameName').value.trim();
+
+    // Regex pattern to allow only letters and numbers
+    var regex = /^[a-zA-Z0-9\s]*$/;
+
+    if (graalID === '' || inGameName === '') {
+        alert('Please fill in all fields.');
+        return;
+    }
+
+    if (!regex.test(graalID) || !regex.test(inGameName)) {
+        alert('Please enter only letters and numbers.');
+        return;
+    }
+
+    // Check length of input
+    if (graalID.length > 15 || inGameName.length > 15) {
+        alert('Please limit each field to 15 characters or less.');
+        return;
+    }
+
+    // Send form data if validation passes
+    var formData = new FormData(this);
+    fetch('https://script.google.com/macros/s/AKfycbz6ZISVS_gzDOypEkzLWgaby9A9SbswxN9vBY1FcHoU5TzRklWUJLXLSWvZd60BYYmn/exec', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(text => {
+        if (!text.trim()) {
+            throw new Error('Response is empty');
+        }
+        try {
+            const data = JSON.parse(text);
+            if (data.success) {
+                // Display modal
+                document.getElementById('myModal2').style.display = 'block';
+                document.getElementById('myForm').reset(); // Reset the form fields
+            } else {
+                throw new Error('Invalid response from the server');
+            }
+        } catch (error) {
+            throw new Error('Response is not valid JSON');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An unexpected error occurred.');
+    });
+});
+
+// Close the modal when the user clicks on the close button
+document.getElementsByClassName('close2')[0].addEventListener('click', function() {
+    document.getElementById('myModal2').style.display = 'none';
+});
